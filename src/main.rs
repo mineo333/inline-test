221bc100871acaed8f780fa23de02b01b0538de9
--replace-all <--replace-all>
2024-01-18 10:34:12 -0500

    Add error value

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,83 @@
-use inline_c::assert_c;
+use inline_c::assert_cxx;
 use std::env;
 use std::fs;
 use libloading;
 use core::ffi::c_void;
 
 
-unsafe fn test_stdout() {
-    let compiled_lib = assert_c! {
+unsafe fn test_harness() {
+    let compiled_lib = assert_cxx!{
         #inline_c_rs CFLAGS: "-shared"
-        #include <stdio.h>
+        #include <stdint.h>
         #include <stdlib.h>
+        #include <string>
 
-        void uaf() {
-            printf("Hello World\n");
-            int* buf = malloc(32);
-            free(buf);
-            *buf = 0xdeadbeef;
-            
+        extern "C" int heap_uaf_read() {
+            int *array = new int[100];
+            delete[] array;
+            fprintf(stdout, "%d\n", array[5]);
+            return 0;
+        }
+
+        extern "C" int heap_uaf_write() {
+            int *array = new int[100];
+            delete[] array;
+            array[5] = 1;
+            return 0;
+        }
+
+        extern "C" int heap_oob_read() {
+            int *array = new int[100];
+            fprintf(stdout, "%d\n", array[100]);
+            delete[] array;
+            return 0;
+        }
+
+        extern "C" int heap_oob_write() {
+            int *array = new int[100];
+            array[100] = 1;
+            delete[] array;
+             return 0;
+        }
+        extern "C" int malloc_heap_uaf_read() {
+            int *array = static_cast<int *>(malloc(100 * sizeof(int)));
+            free(array);
+            fprintf(stdout, "%d\n", array[5]);
+            return 0;
+        }
+
+        extern "C" int malloc_heap_uaf_write() {
+            int *array = static_cast<int *>(malloc(100 * sizeof(int)));
+            free(array);
+            array[5] = 1;
+            return 0;
+        }
+
+        extern "C" int malloc_heap_oob_read() {
+            int *array = static_cast<int *>(malloc(100 * sizeof(int)));
+            fprintf(stdout, "%d\n", array[100]);
+            free(array);
+            return 0;
+        }
+
+        extern "C" int malloc_heap_oob_write() {
+            int *array = static_cast<int *>(malloc(100 * sizeof(int)));
+            array[100] = 1;
+            free(array);
+            return 0;
+        }
+
+        extern "C" int LLVMFuzzerTestOneInput() {
+            // abort();
+            return 0;
         }
     };
 
     println!("{:?}", compiled_lib.output_path());
+
+    loop {
+
+    }
     let lib = libloading::Library::new(compiled_lib.output_path()).expect("Failed to load library");
     let symbol: libloading::Symbol<unsafe extern fn() -> c_void> = lib.get(b"uaf").expect("Failed to find uaf");
     //println!("{:#x}", symbol.into_raw().into_raw() as u64);
@@ -34,6 +91,6 @@ unsafe fn test_stdout() {
 
 fn main() {
 
-    unsafe { test_stdout() };    
+    unsafe { test_harness() };    
     
 }
